f6758a354d0df6e33e3879c6345d4ba9a41ecd83
Kieran Rimmer <kieranrimmer@gmail.com>
2018-04-15 10:48:18 +1000

    removed some dross and updated README

--- frames.py
+++ frames.py
@@ -8,30 +8,12 @@ def read_rectangles_from_text_file(filename):
 
 def main():
 
-    # rectangle_list = [Rectangle(1, 3, 1, 3), Rectangle(2, 4, 2, 4), Rectangle(5, 7, 5, 7)] # 20
-    # rectangle_list = [Rectangle(1, 3, 1, 3)]
-    # rectangle_list = [Rectangle(0, 10, 0, 10), Rectangle(1, 3, 1, 3), Rectangle(2, 4, 2, 4), Rectangle(3, 5, 3, 5)]
-    # rectangle_list = [Rectangle(1, 3, 1, 3), Rectangle(5, 7, 5, 7)]
-    # rectangle_list = [Rectangle(1, 3, 1, 3), Rectangle(2, 4, 2, 4), Rectangle(3, 5, 3, 5)]
-    # rectangle_list = [Rectangle(1, 3, 1, 3), Rectangle(2, 4, 2, 4)]
-
-    rectangle_list = [Rectangle(1, 3, 1, 3), Rectangle(2, 4, 2, 4), Rectangle(1, 5, 2, 4)]
-
-    p = perimeter_of_union_of_rectangles(rectangle_list)
-    print "Perimeter of union:", p
-
-    m = measure_of_union_of_rectangles(rectangle_list)
-    print "Measure of union:", m
-
-
     frame_1_filename = 'frames_1.txt' 
     rectangle_list_1 = read_rectangles_from_text_file(frame_1_filename)
 
     p_1 = perimeter_of_union_of_rectangles(rectangle_list_1)
     print "Perimeter of union for file '{}': {}".format(frame_1_filename, p_1)
 
-    m_1 = measure_of_union_of_rectangles(rectangle_list_1)
-    print "Measure of union:", m_1
 
     frame_2_filename = 'frames_2.txt' 
 
@@ -40,12 +22,7 @@ def main():
     p_2 = perimeter_of_union_of_rectangles(rectangle_list_2)
     print "Perimeter of union for file '{}': {}".format(frame_2_filename, p_2)
 
-    m_2 = measure_of_union_of_rectangles(rectangle_list_2)
-    print "Measure of union:", m_2
 
-    # vertical_edges, horizontal_edgs = contour_of_union_of_rectangles(rectangle_list)
-    # print "Vertical Edges in Contour:", vertical_edges
-    # print "Horizontal Edges in Contour:",  horizontal_edgs
 
 
 if __name__ == "__main__":
